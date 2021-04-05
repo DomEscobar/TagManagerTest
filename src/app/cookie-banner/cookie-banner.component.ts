9256ce18f20f32ff532c36e3b868f0137b9bd886
domEscobar <d.hueckmann@googlemail.com>
2021-04-05 15:59:25 +0200

    Import

---
+++ src/app/cookie-banner/cookie-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie-banner',
+  templateUrl: './cookie-banner.component.html',
+  styleUrls: ['./cookie-banner.component.css']
+})
+export class CookieBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
