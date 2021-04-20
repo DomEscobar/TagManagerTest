b7c42a41ae7e523459b0c299a8100d6001a56fa4
Dominic Hueckmann <dominic.hueckmann@camao.one>
2021-04-20 13:45:51 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxGoogleAnalyticsModule.forRoot('G-NYLE32TNYR')
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GaActionEnum, GoogleAnalyticsService } from "ngx-google-analytics";
 import { GaEcommerceItem } from "../google-analysis-4/ga-ecommerce-item";
 import { GaEvent } from "../google-analysis-4/ga-events.enum";
 import { GtmService, GtmTagData, GtmEvent, GtmTagEcommerceData } from "../google-analysis-4/gtm.service";
@@ -10,22 +11,53 @@ declare const gtag: any;
 })
 export class Page1Component implements OnInit {
 
-  constructor(private readonly _gtmService: GtmService) { }
+  constructor(
+    private $gaService: GoogleAnalyticsService,
+    private readonly _gtmService: GtmService) { }
 
   ngOnInit(): void {
-
   }
 
   form1() {
-    this._gtmService.pushDatalayer(
-      new GtmTagEcommerceData(
-        GaEvent.ViewItemList,
-        [
-          new GaEcommerceItem("test", "test", "test", "test", "test", "1")
-        ]
-      )
-    );
 
+    gtag('event', 'video_auto_play_start', {
+      'event_label': 'My promotional video',
+      'event_category': 'video_auto_play'
+    });
+
+    gtag('event', 'view_item_list', {
+      'items': [
+        {
+          'item_name': 'TESRSRESRS',       // Name or ID is required.
+          'item_id': '12345',
+          'price': '15.25',
+          'item_brand': 'Google',
+          'item_category': 'Apparel',
+          'item_category2': 'Mens',
+          'item_category3': 'Shirts',
+          'item_category4': 'Tshirts',
+          'item_variant': 'Gray',
+          'item_list_name': 'Search Results',
+          'item_list_id': 'SR123',
+          'index': 1,
+          'quantity': '1'
+        },
+        {
+          'item_name': 'TESRSRESRS Friday Scented T-Shirt',
+          'item_id': '67890',
+          'price': '33.75',
+          'item_brand': 'Google',
+          'item_category': 'Apparel',
+          'item_category2': 'Mens',
+          'item_category3': 'Shirts',
+          'item_category4': 'Tshirts',
+          'item_variant': 'Black',
+          'item_list_name': 'Search Results',
+          'item_list_id': 'SR123',
+          'index': 2,
+          'quantity': '1'
+        }]
+    });
 
     gtag({
       'event': 'view_item_list',
