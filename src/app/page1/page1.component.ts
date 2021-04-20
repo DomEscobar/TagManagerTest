6656cdfa96e217e83e60e981304e9b5685b57cf6
Dominic Hueckmann <dominic.hueckmann@camao.one>
2021-04-20 13:51:06 +0200

    test2

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -19,13 +19,9 @@ export class Page1Component implements OnInit {
   }
 
   form1() {
-
-    gtag('event', 'video_auto_play_start', {
-      'event_label': 'My promotional video',
-      'event_category': 'video_auto_play'
-    });
-
     gtag('event', 'view_item_list', {
+      'event_name': 'Ich bin name 2',
+      'event_category': 'Click',
       'items': [
         {
           'item_name': 'TESRSRESRS',       // Name or ID is required.
@@ -57,43 +53,6 @@ export class Page1Component implements OnInit {
           'index': 2,
           'quantity': '1'
         }]
-    });
-
-    gtag({
-      'event': 'view_item_list',
-      'ecommerce': {
-        'items': [
-          {
-            'item_name': 'Triblend Android T-Shirt',       // Name or ID is required.
-            'item_id': '12345',
-            'price': '15.25',
-            'item_brand': 'Google',
-            'item_category': 'Apparel',
-            'item_category2': 'Mens',
-            'item_category3': 'Shirts',
-            'item_category4': 'Tshirts',
-            'item_variant': 'Gray',
-            'item_list_name': 'Search Results',
-            'item_list_id': 'SR123',
-            'index': 1,
-            'quantity': '1'
-          },
-          {
-            'item_name': 'Donut Friday Scented T-Shirt',
-            'item_id': '67890',
-            'price': '33.75',
-            'item_brand': 'Google',
-            'item_category': 'Apparel',
-            'item_category2': 'Mens',
-            'item_category3': 'Shirts',
-            'item_category4': 'Tshirts',
-            'item_variant': 'Black',
-            'item_list_name': 'Search Results',
-            'item_list_id': 'SR123',
-            'index': 2,
-            'quantity': '1'
-          }]
-      }
     });
   }
 }
